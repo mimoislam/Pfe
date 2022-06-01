ed0fcdba3030b2be96ca8e7b1379de82c50e1c52
mimoislam <benoaie.islam@gmail.com>
2022-06-01 22:26:42 +0100

    added

--- Database/migrations/2022_05_30_140047_create_play_books_table.php
+++ Database/migrations/2022_05_30_140047_create_play_books_table.php
@@ -19,6 +19,8 @@ return new class extends Migration
             $table->string('system');
             $table->integer('user_id');
 
+
+
             $table->string('githubUrl');
             $table->string('description');
 
